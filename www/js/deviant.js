ee98b098ab8d4bc204256e0dd6a3d2e8a837d561
LeRoy Ellis <leroy.w.ellis@gmail.com>
2015-03-27 13:43:06 -0600

    Updates to add more functionality to the slideshow.

--- www/js/deviant.js
+++ www/js/deviant.js
@@ -1,12 +1,60 @@
 DEVIANT = {
 
 	images: [],
-	current: 0,
 	pauseTime: 3000,
+	current: 0,
+	offset: 0,
+
+	launchFullscreen: function ( element )
+	{
+		if( element.requestFullscreen )
+		{
+		  element.requestFullscreen();
+		}
+		else if( element.mozRequestFullScreen )
+		{
+		  element.mozRequestFullScreen();
+		}
+		else if( element.webkitRequestFullscreen )
+		{
+		  element.webkitRequestFullscreen();
+		}
+		else if( element.msRequestFullscreen )
+		{
+		  element.msRequestFullscreen();
+		}
+		else
+		{
+			console.log( "EAD!" );
+		}
+	},
+
+	exitFullscreen: function()
+	{
+		if( document.exitFullscreen )
+		{
+		  document.exitFullscreen();
+		}
+		else if( document.mozCancelFullScreen )
+		{
+		  document.mozCancelFullScreen();
+		}
+		else if( document.webkitExitFullscreen )
+		{
+		  document.webkitExitFullscreen();
+		}
+	},
 
 	init: function()
 	{
+		this.launchFullscreen( $("#content_item" ) );
+		this.startSlideShow();
+	},
+
+	startSlideShow: function()
+	{
 		images = [];
+		current = 0;
 		$.when( this.loadImages() )
 		 .then( function() {
 			 DEVIANT.showImages();
@@ -18,10 +66,12 @@ DEVIANT = {
 		var dfr = $.Deferred();
 
 		//var site = "http://backend.deviantart.com/rss.xml?q=special:dd";
-		var site = "http://backend.deviantart.com/rss.xml";
+		var site = "http://backend.deviantart.com/rss.xml?q=boost%3Apopular+meta%3Aall+max_age%3A24h&amp;type=deviation&offset=" + this.offset;
+		console.log( site );
 		var url = "http://query.yahooapis.com/v1/public/yql?q=" +
 				  encodeURIComponent('select * from xml where url="' + site + '"') +
 				  "&format=xml&callback=?";
+//		console.log( url );
 
 		$.getJSON( url )
 		 .done( function( data ) {
@@ -56,7 +106,8 @@ DEVIANT = {
 		}
 		else
 		{
-			this.init();
+			this.offset += this.current;
+			this.startSlideShow();
 		}
 	},
 
